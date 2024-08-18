2a3c6e504c921e39b3af1ca097ac0039198986c1
randombishop <nabil.abdellaoui@expr-research.com>
2024-08-18 19:45:37 -0400

    initial commit

---
+++ bots/iaction.py
@@ -0,0 +1,16 @@
+class IAction:
+    
+    def __init__(self, params):
+        self.params = params
+
+    def get_cost(self):
+        """Calculate and return the cost of executing the action."""
+        raise NotImplementedError("Action doesn't implement get_cost")
+
+    def execute(self):
+        """Execute the action."""
+        raise NotImplementedError("Action doesn't implement execute")
+
+    def get_casts(self):
+        """Return the associated casts."""
+        raise NotImplementedError("Action doesn't implement get_casts")
