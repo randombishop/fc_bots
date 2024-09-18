619de449302969d2364eed2a4aa4b691d7dff0cf
randombishop <nabil.abdellaoui@expr-research.com>
2024-09-17 21:24:40 -0400

    prompt adjustment

--- bots/router.py
+++ bots/router.py
@@ -18,15 +18,15 @@ intro = """
 INSTRUCTIONS:
 Map the query to one of the following actions.
 Your goal is not to answer the query.
-Just find the function that should be used to reply to the query.
+Just find the action that matches the user intent.
 """
 
 format = """
 RESPONSE FORMAT:
 {
-  "function": function_number_from_the_provided_list
+  "function": one of [?]}
 }
-"""
+""".replace('?', ','.join([str(x) for x in ACTIONS.keys()]))
 
 instructions = intro + '\n' + DESCRIPTIONS + '\n' + format
 
@@ -54,7 +54,9 @@ def route(request, fid_origin=None, parent_hash=None):
 
 if __name__ == "__main__":
   request = sys.argv[1]
-  action = route(request)
+  fid_origin = sys.argv[2] if len(sys.argv) > 2 else None
+  parent_hash = sys.argv[3] if len(sys.argv) > 3 else None
+  action = route(request, fid_origin=fid_origin, parent_hash=parent_hash)
   print(action)
   action.get_cost()
   print(f"Cost: {action.cost}")
