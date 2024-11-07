b3537eab3ea429c2574d11a07a873ae221e9c908
randombishop <nabil.abdellaoui@expr-research.com>
2024-11-07 08:46:16 -0500

    moving to dune 3/4

--- bots/data/users.py
+++ bots/data/users.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 load_dotenv()
-import sys
-import time
+import json
 from bots.data.wield import get_user_info_by_fid, get_user_info_by_name
+from bots.data.dune import run_query
+from dune_client.types import QueryParameter
 
 
 def get_username(fid):
@@ -20,6 +21,29 @@ def get_fid(username):
   else:
     return None
 
+
+def get_words_dict(fid):
+  query_id = 4257892
+  params = [QueryParameter.number_type(name="fid", value=fid)]
+  response = run_query(query_id, params)
+  rows = response.to_dict('records')
+  if len(rows) == 0:
+    return None
+  else:
+    s = rows[0]['keywords']
+    if s is None:
+      return None
+    else:
+      return json.loads(s)
+
+
+def get_favorite_users(fid):
+  query_id = 4258114
+  params = [
+    QueryParameter.number_type(name="fid", value=fid),
+    QueryParameter.number_type(name="limit", value=10)
+  ]
+  return run_query(query_id, params)
 
 
 if __name__ == "__main__":
