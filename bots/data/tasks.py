d7beb2f7d166a9b64c8405fe6a93ca597d2bea18
randombishop <nabil.abdellaoui@expr-research.com>
2024-11-08 09:49:16 -0500

    fetch and update mention_listeners

--- bots/data/tasks.py
+++ bots/data/tasks.py
@@ -1,7 +1,7 @@
 import json
 from bots.data.pg import pg_connection
 
-
+  
 def get_task(task_id):
   with pg_connection.cursor() as cursor:
     cursor.execute('SELECT * FROM ds.tasks WHERE id = %s', (task_id,))
@@ -14,4 +14,20 @@ def update_task_result(task_id, result):
       "UPDATE ds.tasks SET result = %s, done_at = now() WHERE id = %s",
       (json.dumps(result), task_id)
     )
+    pg_connection.commit()
+
+
+def get_mention_listener_next_page(fid):
+  with pg_connection.cursor() as cursor:
+    cursor.execute('SELECT next_page FROM ds.mention_listeners WHERE fid = %s', (fid,))
+    rows = cursor.fetchall()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0][0]
+    
+
+def update_mention_listener_next_page(fid, next_page):
+  with pg_connection.cursor() as cursor:
+    cursor.execute('UPDATE ds.mention_listeners SET next_page = %s WHERE fid = %s', (next_page, fid))
     pg_connection.commit()
