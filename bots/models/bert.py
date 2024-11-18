25998b8cd94f138e8c99296d99947b8b5588f6fc
randombishop <nabil.abdellaoui@expr-research.com>
2024-11-18 12:16:31 -0500

    bert path

--- bots/models/bert.py
+++ bots/models/bert.py
@@ -3,15 +3,14 @@ load_dotenv()
 import os
 import tensorflow as tf
 import tensorflow_text as tf_text
-import tensorflow_hub as hub
 
 
 print('Loading Tensorflow models...')
 model_dir = os.path.dirname(os.path.abspath(__file__))
-model_preprocess = model_dir + '/bert-tensorflow2-multi-cased-preprocess-v3'
-model_handle = model_dir + '/https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/3'
+model_preprocess = model_dir + '/tf_hub/bert-tensorflow2-multi-cased-preprocess-v3'
+model_handle = model_dir + '/tf_hub/bert-tensorflow2-multi-cased-l-12-h-768-a-12-v3'
 bert_preprocess_model = tf.saved_model.load(model_preprocess)
-bert_model = hub.KerasLayer(model_handle)
+bert_model = tf.saved_model.load(model_handle)
 print('Tensorflow embedding model loaded.')
 
 
