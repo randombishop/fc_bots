7f4a753edd8cdfc2da0d1434d59236cdbd54b642
randombishop <nabil.abdellaoui@expr-research.com>
2024-11-18 12:19:48 -0500

    bird path

--- bots/models/bird.py
+++ bots/models/bird.py
@@ -8,7 +8,7 @@ import shap
 print('Loading bird model...')
 bird = xgboost.Booster() 
 model_dir = os.path.dirname(os.path.abspath(__file__))
-bird.load_model(model_dir + 'bird_0_0_1.xgb.json')
+bird.load_model(model_dir + '/' +'bird_0_0_1.xgb.json')
 bird_explainer = shap.TreeExplainer(bird)
 features = bird.feature_names
 print('Bird model loaded.')
