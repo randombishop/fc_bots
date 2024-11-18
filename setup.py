e6b3bff032a437f4e76861cb74be9349e05cc90c
randombishop <nabil.abdellaoui@expr-research.com>
2024-11-18 08:45:59 -0500

    setup

--- setup.py
+++ setup.py
@@ -15,8 +15,11 @@ setup(
       "ollama==0.2.1",
       "wordcloud==1.9.3",
       "matplotlib==3.9.2",
+      "nltk==3.9.1",
+      "langdetect==1.0.9",  
       "dune_client==1.7.7",
       "google-cloud-storage==2.18.2",
+      "google-cloud-aiplatform==1.59.0",
       "python-dotenv==1.0.1"
     ]
 )
