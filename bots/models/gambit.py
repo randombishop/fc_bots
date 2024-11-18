7f45747d724312332edd8b1059726b2e27327743
randombishop <nabil.abdellaoui@expr-research.com>
2024-11-18 15:31:49 -0500

    test gambit

--- bots/models/gambit.py
+++ bots/models/gambit.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
 import os
-from google.cloud import pubsub_v1
 import tensorflow as tf
 import pandas
 import json
@@ -13,13 +12,17 @@ model_source_meta = 'gambit2.json'
 model_dir = os.path.dirname(os.path.abspath(__file__))
 with open(model_dir  + '/' +  model_source_meta) as f:
   meta = json.load(f)
-gambit = tf.keras.models.load_model(model_dir + '/' + model_source_h5)
+questions = meta['questions']
+categories = meta['categories']
+topics = meta['topics']
+encoder = meta['encoder']
+gambit_model = tf.keras.models.load_model(model_dir + '/' + model_source_h5)
 print('Gambit model loaded.')
-gambit.summary()
+gambit_model.summary()
 
 
-def transform(input):
-  g = gambit.predict(input)
+def gambit(input):
+  g = gambit_model.predict(input)
   df_q = pandas.DataFrame(g[0], columns=meta['questions'])
   df_cats = pandas.DataFrame(g[1], columns=meta['categories'])
   df_topics = pandas.DataFrame(g[2], columns=meta['topics'])
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,6 @@
 import unittest
-from bots.models.bert import preprocess, bert
+from bots.models.bert import bert
+from bots.models.gambit import gambit, categories, topics
 
 
 class TestModels(unittest.TestCase):
@@ -11,5 +12,21 @@ class TestModels(unittest.TestCase):
         "Zowie! May today be the start of something beautiful in your life.   "
     ]
     result = bert(sentences)
-    print(result)
+    self.assertEqual(result.shape[0], 3)
+    self.assertEqual(result.shape[1], 512)
     
+  def test_gambit(self):
+    sentences = [
+        "gm crypto folks! long live #bitcoin the king!",
+        "Pink Floyd are great artists",
+        "Let's talk about football: who is the greatest player of all time?"
+    ]
+    expected_categories = ['c_crypto', 'c_arts', 'c_sports']
+    expected_topics = ['t_bitcoin', 't_music', 't_football']
+    embed = bert(sentences)
+    df = gambit(embed)
+    category_label = list(df[categories].idxmax(axis=1))
+    topic_label = list(df[topics].idxmax(axis=1))
+    for i in range(len(sentences)):
+      self.assertEqual(category_label[i], expected_categories[i])
+      self.assertEqual(topic_label[i], expected_topics[i])
