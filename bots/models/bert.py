17aec213bf1c7b1f24162addd77539880958576e
randombishop <nabil.abdellaoui@expr-research.com>
2024-11-18 09:47:33 -0500

    use local copy of a model

--- bots/models/bert.py
+++ bots/models/bert.py
@@ -1,14 +1,16 @@
 from dotenv import load_dotenv
 load_dotenv()
+import os
 import tensorflow as tf
 import tensorflow_text as tf_text
 import tensorflow_hub as hub
 
 
 print('Loading Tensorflow models...')
-model_preprocess = 'https://tfhub.dev/tensorflow/bert_multi_cased_preprocess/3'
-model_handle = 'https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/3'
-bert_preprocess_model = hub.KerasLayer(model_preprocess)
+model_dir = os.path.dirname(os.path.abspath(__file__))
+model_preprocess = model_dir + '/bert-tensorflow2-multi-cased-preprocess-v3'
+model_handle = model_dir + '/https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/3'
+bert_preprocess_model =tf.keras.models.load_model(model_preprocess)
 bert_model = hub.KerasLayer(model_handle)
 print('Tensorflow embedding model loaded.')
 
