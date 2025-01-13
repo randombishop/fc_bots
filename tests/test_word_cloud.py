0650b674fb994d05af403adc5144fac42fe6e559
randombishop <nabil.abdellaoui@expr-research.com>
2025-01-12 19:36:34 -0500

    word cloud tests

--- tests/test_word_cloud.py
+++ tests/test_word_cloud.py
@@ -27,3 +27,68 @@ class TestFavoriteUsers(unittest.TestCase):
     self.assertEqual(action.user_name, 'randombishop')
     self.assertEqual(len(action.casts), 1)
     self.assertEqual(len(action.casts[0]['embeds']), 1)
+    
+  def test3(self):
+    request = "Make a word cloud for keyword 'bitcoin'"
+    action = route(request)
+    action.run()
+    action.print()
+    self.assertIsInstance(action, WordCloud)
+    self.assertEqual(action.keyword, 'bitcoin')
+    self.assertIsNone(action.channel)
+    self.assertIsNone(action.user_name)
+    self.assertIsNone(action.search)
+    self.assertEqual(len(action.casts), 1)
+    self.assertEqual(len(action.casts[0]['embeds']), 1)
+    
+  def test4(self):
+    request = "Make a wordcloud for arts category"
+    action = route(request)
+    action.run()
+    action.print()
+    self.assertIsInstance(action, WordCloud)
+    self.assertEqual(action.category, 'c_arts')
+    self.assertIsNone(action.channel)
+    self.assertIsNone(action.keyword)
+    self.assertIsNone(action.user_name)
+    self.assertIsNone(action.search)
+    self.assertEqual(len(action.casts), 1)
+    self.assertEqual(len(action.casts[0]['embeds']), 1)
+    
+  def test5(self):
+    request = "Make a wordcloud for /data channel?"
+    action = route(request)
+    action.run()
+    action.print()
+    self.assertIsInstance(action, WordCloud)
+    self.assertEqual(action.channel, 'https://farcaster.group/data')
+    self.assertIsNone(action.keyword)
+    self.assertIsNone(action.category)
+    self.assertIsNone(action.user_name)
+    self.assertIsNone(action.search)
+    self.assertEqual(len(action.casts), 1)
+    self.assertEqual(len(action.casts[0]['embeds']), 1)
+
+  def test6(self):
+    request = "Make a word cloud about the beaty of canadian landscapes"
+    action = route(request)
+    action.run()
+    action.print()
+    self.assertIsInstance(action, WordCloud)
+    self.assertIsNotNone(action.search)
+    self.assertEqual(len(action.casts), 1)
+    self.assertEqual(len(action.casts[0]['embeds']), 1)
+      
+  def test7(self):
+    request = "Make a wordcloud for @randombishop's posts"
+    action = route(request)
+    action.run()
+    action.print()
+    self.assertIsInstance(action, WordCloud)
+    self.assertEqual(action.user_name, 'randombishop')
+    self.assertIsNone(action.keyword)
+    self.assertIsNone(action.category)
+    self.assertIsNone(action.channel)
+    self.assertIsNone(action.search)
+    self.assertEqual(len(action.casts), 1)
+    self.assertEqual(len(action.casts[0]['embeds']), 1)
