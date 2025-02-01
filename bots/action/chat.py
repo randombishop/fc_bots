1615097e568141c540cfe6318e8d16b0b2539983
randombishop <nabil.abdellaoui@expr-research.com>
2025-02-01 16:36:04 -0500

    Chat action prototype, prompt to be improved...

--- bots/action/chat.py
+++ bots/action/chat.py
@@ -5,6 +5,7 @@ from bots.utils.check_links import check_link_data
 
 chat_instructions_template = """
 You are @{{name}}, a social media bot.
+Your goal is to reply to a user conversation with a creative tweet with a reference to one of the provided posts.
 
 #YOUR BIO
 {{bio}}
@@ -20,9 +21,10 @@ You are @{{name}}, a social media bot.
 
 #TASK
 Respond to the user conversation with a tweet and a link to a post.
-First, pick a post from the provided ones that is somehow related to the conversation.
-The selected post doesn't have to be directly related to the conversation, it can be used as a metaphor, funny reference, opposite thing.
-You can also select a post as to connect another user to the conversation and tweet a haiku for both of them.
+Pick a post from the provided ones that is somehow related to the conversation.
+Pick a post that has a meaningful, metaphorical, humorous, or even contrasting connection to the conversation.
+The selected post doesn't have to be directly related to the conversation, it can be any idea that you can play with when replying to the user.
+You can also select a post that connects another user to the conversation and tweet a haiku for both of them.
 Be creative in picking a post and select one that will be useful to include in your answer.
 Once you found the ideal post to use, respond to the user conversation with a tweet and a link to the post.
 If you can't find any post that can be used that way, respond with a tweet only and no link.
@@ -83,11 +85,16 @@ class Chat(IActionStep):
     chat_prompt = self.state.format(chat_prompt_template)
     chat_instructions = self.state.format(chat_instructions_template)
     result = call_llm(chat_prompt, chat_instructions, chat_schema)
-    print('result', result)
+    if 'tweet' not in result or result['tweet'] is None or len(result['tweet']) < 2:
+      raise Exception('Could not generate a response.')
     link = None
     if 'link_id' in result:
       link = check_link_data({'id':result['link_id']}, self.state.posts_map)
-    print('link', link)      
-    casts = []
+    cast = {
+      'text': result['tweet'],
+      'embeds': [{'fid': link['fid'], 'user_name': link['user_name'], 'hash': link['hash']}]
+    } 
+    print(f"https://warpcast.com/{link['user_name']}/{link['hash']}")
+    casts = [cast]
     check_casts(casts)
     self.state.casts = casts
--- tests/test_chat.py
+++ tests/test_chat.py
@@ -4,14 +4,13 @@ from bots.utils.tests import make_bot
 
 def assert_expected_output(t, bot, expected_continue):
   action = bot.state.selected_action
-  should_continue = bot.state.should_continue
   if expected_continue:
-    t.assertTrue(action == 'Chat' or action == 'Perplexity')
-    t.assertTrue(should_continue)
+    t.assertTrue(action == 'Chat')
+    t.assertEqual(len(bot.state.casts), 1)
   else:
     t.assertTrue(action is None or action == 'Chat')
-    if action == 'Chat':
-      t.assertFalse(should_continue)
+    t.assertEqual(len(bot.state.casts), 0)
+
 
 class TestChat(unittest.TestCase):
     
@@ -23,28 +22,21 @@ class TestChat(unittest.TestCase):
     assert_expected_output(self, bot, False)
 
   def test2(self):
-    request = "Who are you?"
+    request = "Chat with me: Who are you?"
     bot = make_bot()
     bot.respond(request)
     bot.state.debug_action()
     assert_expected_output(self, bot, True)
 
   def test3(self):
-    request = "How old are you?"
-    bot = make_bot()
-    bot.respond(request)
-    bot.state.debug_action()
-    assert_expected_output(self, bot, True)
-
-  def test4(self):
-    request = "Do you think DeepSeek will surpass OpenAI?"
+    request = "Chat with me: Do you think DeepSeek will surpass OpenAI?"
     fid_origin=253232
     bot = make_bot()
     bot.respond(request, fid_origin=fid_origin)
     bot.state.debug_action()  
     assert_expected_output(self, bot, True)
 
-  def test5(self):
+  def test4(self):
     request = "Thank you so much for sharing 😺❤️❤️❤️"
     fid_origin=388401
     parent_hash='0x7506e607e722d543b61306d1357814ad61caa132'
