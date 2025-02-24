e20c386169042cbe124594fb0673fd3cbda85962
randombishop <nabil.abdellaoui@expr-research.com>
2025-02-24 12:37:55 -0500

    init.py

--- bots/memory/memory_steps.py
+++ bots/memory/memory_steps.py
@@ -1,19 +1,7 @@
-from bots.prepare.get_bot_casts import GetBotCasts
-from bots.prepare.get_bot_casts_in_channel import GetBotCastsInChannel
-from bots.prepare.get_bot_casts_no_channel import GetBotCastsNoChannel
-from bots.prepare.get_casts import GetCasts
-from bots.prepare.get_casts_in_channel import GetCastsInChannel
-from bots.prepare.get_trending import GetTrending
-from bots.prepare.should_continue import ShouldContinue
+from bots.memory.user_profile import UserProfile
 
 
-PREPARE_STEPS = {
-  'GetBotCasts': GetBotCasts,
-  'GetBotCastsInChannel': GetBotCastsInChannel,
-  'GetBotCastsNoChannel': GetBotCastsNoChannel,
-  'GetCasts': GetCasts,
-  'GetCastsInChannel': GetCastsInChannel,
-  'GetTrending': GetTrending,
-  'ShouldContinue': ShouldContinue
+MEMORY_STEPS = {
+  'user_profile': UserProfile
 }
 
