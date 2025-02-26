28ea876aa4f9b50237eccae2016c15067baa1488
randombishop <nabil.abdellaoui@expr-research.com>
2025-02-26 17:12:07 -0500

    log prompt errors in openai

--- bots/utils/openai.py
+++ bots/utils/openai.py
@@ -1,12 +1,17 @@
 from openai import OpenAI
 
 def generate_image(prompt):
-  client = OpenAI()
-  response = client.images.generate(
-      model="dall-e-3",
-      prompt=prompt,
-      size="1024x1024",
-      quality="standard",
-      n=1,
-  )
-  return response.data[0].url
+  try:
+    client = OpenAI()
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+    return response.data[0].url
+  except Exception as e:
+    print('openai image generation failed for following prompt:')
+    print(prompt)
+    raise e
