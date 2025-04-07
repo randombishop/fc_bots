82db5d901ba6301c9a56bcac879669823710dbe7
randombishop <nabil.abdellaoui@expr-research.com>
2025-04-07 09:01:29 -0400

    vertex ai safety settings

--- bots/utils/llms2.py
+++ bots/utils/llms2.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 import json5
-from langchain_google_vertexai import ChatVertexAI
+from langchain_google_vertexai import ChatVertexAI, HarmCategory, HarmBlockThreshold
 from langchain_core.messages import HumanMessage, SystemMessage
 from langsmith import traceable
 from bots.utils.json_cleaner import clean_json
@@ -9,14 +9,12 @@ from bots.utils.json_cleaner import clean_json
 
 def get_llm():
   model = "gemini-2.0-flash-001"
-  llm = ChatVertexAI(model=model, safety_settings=[
-    {"category": "HARM_CATEGORY_DEROGATORY", "threshold": 4},
-    {"category": "HARM_CATEGORY_TOXICITY", "threshold": 4},
-    {"category": "HARM_CATEGORY_MEDICAL", "threshold": 4},
-    {"category": "HARM_CATEGORY_VIOLENCE", "threshold": 3},
-    {"category": "HARM_CATEGORY_SEXUAL", "threshold": 2},
-    {"category": "HARM_CATEGORY_DANGEROUS", "threshold": 2}
-  ])
+  llm = ChatVertexAI(model=model, safety_settings={
+    HarmCategory.HARM_CATEGORY_UNSPECIFIED: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+  })
   return llm
 
 
