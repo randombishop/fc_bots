a55c10e98edf7593acdf9241881bd620f0204bb0
randombishop <nabil.abdellaoui@expr-research.com>
2025-05-07 13:12:20 -0400

    bugs

--- bots/kit_interface/most_active_users.py
+++ bots/kit_interface/most_active_users.py
@@ -12,7 +12,7 @@ class MostActiveUsers:
     self.df = df
       
   def __str__(self) -> str:
-    ans += f"Most active users in channel {self.channel_id.channel}\n"
+    ans = f"Most active users in channel {self.channel_id.channel}\n"
     if len(self.df) > 0:
       rows = self.df.to_dict('records')
       for r in rows:
--- tests/test_intents.py
+++ tests/test_intents.py
@@ -9,65 +9,60 @@ class TestIntents(unittest.TestCase):
   
   def test_favorite_users(self):
     request = "Who are @vitalik.eth's favorite users?"
-    state = run_agent(test_id='TestActionSelection:favorite_users', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'FavoriteUsers')
+    state = run_agent(test_id='TestIntents:favorite_users', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'FavoriteUsers')
     
   def test_more_like_this(self):
     request = "More Like This: #Bitcoin is sweet!"
-    state = run_agent(test_id='TestActionSelection:more_like_this', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'MoreLikeThis')
+    state = run_agent(test_id='TestIntents:more_like_this', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'MoreLikeThis')
     
   def test_most_active_users(self):
     request = "Who is most active in channel /data?"
-    state = run_agent(test_id='TestActionSelection:most_active_users', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'MostActiveUsers')
+    state = run_agent(test_id='TestIntents:most_active_users', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'MostActiveUsers')
     
   def test_news(self):
     request = "Data Science news"
-    state = run_agent(test_id='TestActionSelection:news', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'News')
-    
-  def test_perplexity(self):
-    request = "Ask perplexity to compare Farcaster and Bluesky"
-    state = run_agent(test_id='TestActionSelection:perplexity', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'Perplexity')
+    state = run_agent(test_id='TestIntents:news', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'News')
     
   def test_pick(self):
     request = "Pick your favorite cast in channel /mfers"
-    state = run_agent(test_id='TestActionSelection:pick', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'Pick')
+    state = run_agent(test_id='TestIntents:pick', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'Pick')
   
   def test_praise(self):
     request = "Praise @randombishop"
-    state = run_agent(test_id='TestActionSelection:praise', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'Praise')
+    state = run_agent(test_id='TestIntents:praise', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'Praise')
     
   def test_psycho(self):
     request = "Psycho analyze @v"
-    state = run_agent(test_id='TestActionSelection:psycho', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'Psycho')
+    state = run_agent(test_id='TestIntents:psycho', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'Psycho')
     
   def test_roast(self):
     request = "Roast @v"
-    state = run_agent(test_id='TestActionSelection:roast', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'Roast')
+    state = run_agent(test_id='TestIntents:roast', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'Roast')
 
   def test_summary(self):
     request = "Give me a summary about keyword ethereum."
-    state = run_agent(test_id='TestActionSelection:summary', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'Summary')
+    state = run_agent(test_id='TestIntents:summary', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'Summary')
       
   def test_who_is(self):
     request = "Who is @randombishop"
-    state = run_agent(test_id='TestActionSelection:who_is', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'WhoIs')
+    state = run_agent(test_id='TestIntents:who_is', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'WhoIs')
     
   def test_word_cloud(self):
     request = "Make a wordcloud for user @dwr.eth"
-    state = run_agent(test_id='TestActionSelection:word_cloud', mode='bot',request=request)
-    self.assertEqual(state.get('intent'), 'WordCloud')
+    state = run_agent(test_id='TestIntents:word_cloud', mode='bot',request=request)
+    self.assertEqual(state.plan['intent'], 'WordCloud')
     
   def test_user_stats(self):
     request = "How many Brazilians do we have on Farcaster?"
     state = run_agent(test_id='TestUserStats:1', mode='bot', request=request)
-    self.assertEqual(state.get('intent'), 'UserStats')
+    self.assertEqual(state.plan['intent'], 'UserStats')
