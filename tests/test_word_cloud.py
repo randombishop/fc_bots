4f92e627ad717f802eb9dab0994f59152556ff26
randombishop <nabil.abdellaoui@expr-research.com>
2025-05-09 11:30:03 -0400

    tests bot mode

--- tests/test_word_cloud.py
+++ tests/test_word_cloud.py
@@ -39,7 +39,7 @@ class TestWordCloud(unittest.TestCase):
     
   def test4(self):
     request = "Make a wordcloud for /data channel?"
-    state = run_agent(test_id='TestWordCloud:5', mode='bot', request=request)
+    state = run_agent(test_id='TestWordCloud:4', mode='bot', request=request)
     self.assert_expected_output(state)
     channels = state.get_variable_values('ChannelId')
     self.assertEqual(len(channels), 1)
@@ -47,16 +47,16 @@ class TestWordCloud(unittest.TestCase):
     self.assertEqual(channels[0].channel_url, 'https://farcaster.group/data')
 
   def test5(self):
-    request = "Make a word cloud about the beauty of canadian landscapes"
-    state = run_agent(test_id='TestWordCloud:6', mode='bot', request=request)
+    request = "Make a word cloud about the beauty of canada"
+    state = run_agent(test_id='TestWordCloud:5', mode='bot', request=request)
     self.assert_expected_output(state)
     search_phrases = state.get_variable_values('SearchPhrase')
     self.assertEqual(len(search_phrases), 1)
-    self.assertEqual(search_phrases[0].search_phrase, 'the beauty of canadian landscapes')
+    self.assertEqual(search_phrases[0].search_phrase, 'canada')
     
   def test6(self):
     request = "Make a wordcloud for @randombishop's posts"
-    state = run_agent(test_id='TestWordCloud:7', mode='bot', request=request)
+    state = run_agent(test_id='TestWordCloud:6', mode='bot', request=request)
     self.assert_expected_output(state)            
     user = state.get_variable_values('UserId')[-1]
     self.assertEqual(user.fid, 253232)
@@ -65,7 +65,7 @@ class TestWordCloud(unittest.TestCase):
   def test7(self):
     request = "Make a word cloud for this channel"
     root_parent_url = "https://farcaster.group/data"
-    state = run_agent(test_id='TestWordCloud:8', mode='bot', request=request, root_parent_url=root_parent_url)
+    state = run_agent(test_id='TestWordCloud:7', mode='bot', request=request, root_parent_url=root_parent_url)
     self.assert_expected_output(state)
     channels = state.get_variable_values('ChannelId')
     self.assertEqual(channels[-1].channel, 'data')
