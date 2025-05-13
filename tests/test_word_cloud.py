63c2c26a3a03606dc3a6648f1188712240614f14
randombishop <nabil.abdellaoui@expr-research.com>
2025-05-13 12:47:01 -0400

    tests

--- tests/test_word_cloud.py
+++ tests/test_word_cloud.py
@@ -52,7 +52,7 @@ class TestWordCloud(unittest.TestCase):
     self.assert_expected_output(state)
     search_phrases = state.get_variable_values('SearchPhrase')
     self.assertEqual(len(search_phrases), 1)
-    self.assertIn('canada', search_phrases[0].search)
+    self.assertIn('canada', search_phrases[0].search.lower())
     
   def test6(self):
     request = "Make a wordcloud for @randombishop's posts"
