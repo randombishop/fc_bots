d2a492ac7e8ab4f6a3e8bcf205eeb5ffb5387336
randombishop <nabil.abdellaoui@expr-research.com>
2025-07-02 23:13:30 -0400

    verify ssl=false

--- bots/data/dsart.py
+++ bots/data/dsart.py
@@ -2,7 +2,7 @@ import requests
 
 
 def fetch_channels():
-  response = requests.get('https://fc.datascience.art/channels/list')
+  response = requests.get('https://fc.datascience.art/channels/list', verify=False)
   if response.status_code == 200:
     return response.json()
   return []
